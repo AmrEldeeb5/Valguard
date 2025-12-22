1ddfd347c98c72a6b40a6ede60c8889d8d08ce71
amreldeeb <amreldeeb641@gmail.com>
2025-12-22 20:17:12 +0200

    add PortfolioState and UiPortfolioCoinItem for portfolio screen UI state management

---
+++ composeApp/src/commonMain/kotlin/com/example/cryptowallet/app/portfolio/presentation/UiPortfolioCoinItem.kt
@@ -0,0 +1,12 @@
+package com.example.cryptowallet.app.portfolio.presentation
+
+
+data class UiPortfolioCoinItem(
+    val id: String,
+    val name: String,
+    val iconUrl: String,
+    val amountInUnitText: String,
+    val amountInFiatText: String,
+    val performancePercentText: String,
+    val isPositive: Boolean,
+)
